44c618ffab55bd59e44a3e950948f5e7065677cd
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2022-01-19 23:12:53 +0600

    feat: add Action Creators, getting and deleting users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {addCashAction, getCashAction} from "./store/cashReducer";
+
 
 
 function App() {
     const dispatch = useDispatch()
     const cash = useSelector(state => state.cash.cash)
+    const customers = useSelector(state => state.customers.customers)
 
     const addCash = (cash) => {
-        dispatch({type:"ADD_CASH", payload: cash})
+        dispatch(addCashAction(cash))
     }
 
     const getCash = (cash) => {
-        dispatch({type:"GET_CASH", payload: cash})
+        dispatch(getCashAction(cash))
+    }
+
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch(addCustomerAction(customer))
+    }
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id))
     }
 
     return (
@@ -21,6 +37,20 @@ function App() {
             <div style={{display: 'flex'}}>
                 <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
                 <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+            </div>
+            <div>
+                {customers.length > 0 ?
+                    <div className={'cash'}>
+                        {customers.map(customer =>
+                            <div onClick={() => removeCustomer(customer)} className={'customer'}>{customer.name}</div>
+                        )}
+                    </div>
+                    :
+                    <div className={'cash'}>
+                        Пользователи отсутствуют!
+                    </div>
+                }
+                <button onClick={() => addCustomer(prompt())}>Добавить пользователя</button>
             </div>
         </div>
     );
